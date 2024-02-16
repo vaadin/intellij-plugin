6a78a2bffa63975c1d92c6577f800e0e1245b9b4
marcin <marcin@vaadin.com>
2024-02-16 12:35:06 +0100

    feat: Disable action for non Vaadin projects (#4)
    * Disable action for non Vaadin projects

--- src/main/kotlin/com/vaadin/plugin/copilot/action/ToggleServerAction.kt
+++ src/main/kotlin/com/vaadin/plugin/copilot/action/ToggleServerAction.kt
@@ -1,20 +1,13 @@
 package com.vaadin.plugin.copilot.action
 
-import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.vaadin.plugin.copilot.CopilotPluginUtil
 
 class ToggleServerAction : AnAction() {
 
     override fun actionPerformed(p0: AnActionEvent) {
-        val project = p0.project
-        if (project == null) {
-            CopilotPluginUtil.notify("Copilot Plugin can be started only on existing project", NotificationType.WARNING)
-            return
-        }
+        val project = p0.project ?: return
 
         if (CopilotPluginUtil.isServerRunning(project)) {
             CopilotPluginUtil.stopServer(project)
@@ -26,11 +19,13 @@ class ToggleServerAction : AnAction() {
 
     override fun update(e: AnActionEvent) {
         super.update(e)
-
-        e.presentation.isEnabledAndVisible = e.getData(CommonDataKeys.NAVIGATABLE_ARRAY)?.first() is PsiDirectoryNode
-
         val project = e.project
-        if (project != null && CopilotPluginUtil.isServerRunning(project)) {
+        if (project == null || !CopilotPluginUtil.isVaadinProject(project)) {
+            e.presentation.isEnabledAndVisible = false
+            return
+        }
+
+        if (CopilotPluginUtil.isServerRunning(project)) {
             e.presentation.text = "Stop Copilot Plugin"
         } else {
             e.presentation.text = "Start Copilot Plugin"
