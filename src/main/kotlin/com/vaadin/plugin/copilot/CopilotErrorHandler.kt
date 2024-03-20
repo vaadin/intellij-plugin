8e4b9b50322aa9f0786ba159afd1c13d45104d3f
marcin <marcin@vaadin.com>
2024-03-20 13:13:24 +0100

    feat: Report submitter (#15)
    * error handler
    
    * wrong url
    
    * use pluginDescriptor
    
    * do not display empty additional info

---
+++ src/main/kotlin/com/vaadin/plugin/copilot/CopilotErrorHandler.kt
@@ -0,0 +1,51 @@
+package com.vaadin.plugin.copilot
+
+import com.intellij.ide.BrowserUtil
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.diagnostic.ErrorReportSubmitter
+import com.intellij.openapi.diagnostic.IdeaLoggingEvent
+import com.intellij.openapi.diagnostic.SubmittedReportInfo
+import com.intellij.util.Consumer
+import java.awt.Component
+
+
+class CopilotErrorHandler: ErrorReportSubmitter() {
+
+    private val url = "https://github.com/vaadin/intellij-plugin/issues/new"
+
+    override fun getReportActionText(): String {
+        return "Report on GitHub"
+    }
+
+    override fun submit(
+        events: Array<out IdeaLoggingEvent>,
+        additionalInfo: String?,
+        parentComponent: Component,
+        consumer: Consumer<in SubmittedReportInfo>
+    ): Boolean {
+        val throwableText = events.iterator().next().throwableText
+        val firstLine = throwableText.split("\\n".toRegex(), 2)[0].trim()
+        val appName = ApplicationInfo.getInstance().fullApplicationName
+
+        var body = "Plugin version: **${pluginDescriptor.version}**\n" +
+                "IDE version: **$appName**\n\n"
+
+        if (additionalInfo != null) {
+            body += "Additional info:\n" +
+                    "> ${additionalInfo.replace("\\n+".toRegex(), "\n")}" +
+                    "\n\n"
+        }
+
+        body += "Stacktrace:\n" +
+                "```\n" +
+                throwableText +
+                "\n```"
+
+        val title = "[Error report] $firstLine"
+        BrowserUtil.browse("$url?title=$title&body=$body")
+        return true
+    }
+
+
+
+}
