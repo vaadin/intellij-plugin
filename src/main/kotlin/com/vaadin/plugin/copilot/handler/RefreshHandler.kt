3ef09ef013dcabb77939b04af4001af9c19b839a
marcin <marcin@vaadin.com>
2024-04-25 11:34:16 +0200

    refresh file system handler (#36)

---
+++ src/main/kotlin/com/vaadin/plugin/copilot/handler/RefreshHandler.kt
@@ -0,0 +1,12 @@
+package com.vaadin.plugin.copilot.handler
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFileManager
+
+class RefreshHandler(project: Project) : AbstractHandler(project) {
+
+    override fun run() {
+        VirtualFileManager.getInstance().asyncRefresh {  }
+    }
+
+}
