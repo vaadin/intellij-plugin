19a3771173654ec9b241af5afb32cc019507c706
marcin <marcin@vaadin.com>
2024-09-04 16:08:40 +0200

    update messages, hide balloon on go to configuration

--- src/main/kotlin/com/vaadin/plugin/activity/ConfigurationCheckPostStartupProjectActivity.kt
+++ src/main/kotlin/com/vaadin/plugin/activity/ConfigurationCheckPostStartupProjectActivity.kt
@@ -11,25 +11,24 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.vcs.VcsBundle
 import com.intellij.openapi.vcs.VcsConfiguration
 import com.intellij.openapi.vcs.VcsShowConfirmationOption
-import com.intellij.openapi.vcs.configurable.VcsGeneralSettingsConfigurable
 import com.intellij.openapi.vcs.impl.ProjectLevelVcsManagerImpl
-import com.vaadin.plugin.copilot.CopilotPluginUtil
+import com.vaadin.plugin.utils.VaadinIcons
 
 class ConfigurationCheckPostStartupProjectActivity : ProjectActivity {
 
     companion object {
         const val NOTIFICATION_GROUP = "Vaadin Configuration Check"
-        val NOTIFICATION_ICON = IconLoader.getIcon("/icons/vaadin.svg", CopilotPluginUtil::class.java)
 
         val MESSAGE_RELOAD_CLASSES = JavaDebuggerBundle.message("label.debugger.hotswap.configurable.reload.classes")
         val MESSAGE_RELOAD_ALWAYS = JavaDebuggerBundle.message("label.debugger.hotswap.configurable.always")
         val MESSAGE_VCS_CONFIGURABLE = VcsBundle.message("version.control.main.configurable.name")
         val MESSAGE_WHEN_FILES_CREATED = VcsBundle.message("settings.border.when.files.are.created")
         val MESSAGE_ADD_SILENTLY = VcsBundle.message("radio.after.creation.add.silently")
+        val MESSAGE_DO_NOT_ADD = VcsBundle.message("radio.after.creation.do.not.add")
+        val MESSAGE_DONT_ASK_AGAIN = IdeCoreBundle.message("dialog.options.do.not.ask")
     }
 
     override suspend fun execute(project: Project) {
@@ -39,61 +38,63 @@ class ConfigurationCheckPostStartupProjectActivity : ProjectActivity {
 
     private fun checkReloadClassesSetting(project: Project) {
         val dm = DebuggerSettings.getInstance()
-        if (dm.RUN_HOTSWAP_AFTER_COMPILE != DebuggerSettings.RUN_HOTSWAP_ALWAYS) {
-            val action = NotificationAction.create("Go to configuration...", com.intellij.util.Consumer {
-                ShowSettingsUtil.getInstance().showSettingsDialog(project, "reference.idesettings.debugger.hotswap")
-            })
+        if (dm.RUN_HOTSWAP_AFTER_COMPILE == DebuggerSettings.RUN_HOTSWAP_ASK) {
+            notify(
+                project,
+                "Your HotSwap class reload setting requires confirmation each time file is going to be reloaded. " +
+                        "It will cause popups appear while working with Vaadin Copilot. " +
+                        "To improve experience please set \"$MESSAGE_RELOAD_CLASSES\" to \"$MESSAGE_RELOAD_ALWAYS\"",
+                "reference.idesettings.debugger.hotswap"
+            )
+        }
 
-            if (dm.RUN_HOTSWAP_AFTER_COMPILE == DebuggerSettings.RUN_HOTSWAP_ASK) {
-                notify(
-                    "Your HotSwap class reload setting require confirmation each time file is going to be reloaded. " +
-                            "It will cause popups appear while working with Vaadin Copilot. " +
-                            "To improve experience please change \"${MESSAGE_RELOAD_CLASSES}\" to \"${MESSAGE_RELOAD_ALWAYS}\"",
-                    action
-                )
-            }
-
-            if (dm.RUN_HOTSWAP_AFTER_COMPILE == DebuggerSettings.RUN_HOTSWAP_NEVER) {
-                notify(
-                    "Your HotSwap class reload is turned off. It will cause changes are not reflected in your application. " +
-                            "To improve experience please change \"$MESSAGE_RELOAD_CLASSES\" to \"$MESSAGE_RELOAD_ALWAYS\"",
-                    action
-                )
-            }
+        if (dm.RUN_HOTSWAP_AFTER_COMPILE == DebuggerSettings.RUN_HOTSWAP_NEVER) {
+            notify(
+                project,
+                "Your HotSwap class reload is turned off. It will cause changes are not reflected in your application. " +
+                        "To improve experience please set \"$MESSAGE_RELOAD_CLASSES\" to \"$MESSAGE_RELOAD_ALWAYS\"",
+                "reference.idesettings.debugger.hotswap"
+            )
         }
     }
 
     private fun checkVcsAddConfirmationSetting(project: Project) {
         val confirmation = VcsConfiguration.StandardConfirmation.ADD
         val value = ProjectLevelVcsManagerImpl.getInstanceImpl(project).getConfirmation(confirmation).value
-
         if (value == VcsShowConfirmationOption.Value.SHOW_CONFIRMATION) {
-            val action = NotificationAction.create("Go to configuration...", com.intellij.util.Consumer {
-                ShowSettingsUtil.getInstance().showSettingsDialog(project, VcsGeneralSettingsConfigurable::class.java)
-            })
-
             notify(
-                "Your $MESSAGE_VCS_CONFIGURABLE settings require confirmation after creating each new file. " +
+                project,
+                "Your $MESSAGE_VCS_CONFIGURABLE settings requires confirmation after creating each new file. " +
                         "It will cause popups appear while working with Vaadin Copilot. " +
-                        "To improve experience please change \"$MESSAGE_WHEN_FILES_CREATED\" to \"$MESSAGE_ADD_SILENTLY\"",
-                action
+                        "To improve experience please set \"$MESSAGE_WHEN_FILES_CREATED\" to \"$MESSAGE_ADD_SILENTLY\" or \"$MESSAGE_DO_NOT_ADD\"",
+                "project.propVCSSupport.Confirmation"
             )
         }
     }
 
-    private fun notify(content: String, action: AnAction) {
+    private fun notify(project: Project, content: String, configurableId: String) {
         val notification = Notification(
             NOTIFICATION_GROUP,
             content,
             NotificationType.INFORMATION
-        ).setIcon(NOTIFICATION_ICON)
-        notification.addAction(action)
+        ).setIcon(VaadinIcons.VAADIN)
+        notification.addAction(createGoToConfigurationAction(project, configurableId))
         notification.addAction(createDontAskAgainAction())
         Notifications.Bus.notify(notification)
     }
 
+    private fun createGoToConfigurationAction(
+        project: Project,
+        configurableId: String
+    ): AnAction {
+        return NotificationAction.create("Go to configuration...", { _, notification ->
+            notification.hideBalloon()
+            ShowSettingsUtil.getInstance().showSettingsDialog(project, configurableId)
+        })
+    }
+
     private fun createDontAskAgainAction(): NotificationAction {
-        return NotificationAction.create(IdeCoreBundle.message("dialog.options.do.not.ask")) { _, notification ->
+        return NotificationAction.create(MESSAGE_DONT_ASK_AGAIN) { _, notification ->
             notification.expire()
         }
     }
