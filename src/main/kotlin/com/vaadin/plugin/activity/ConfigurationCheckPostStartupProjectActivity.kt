768e2ab17316234c683bd4381453b54c97b13638
marcin <marcin@vaadin.com>
2024-09-05 08:19:23 +0200

    update messages, hide balloon on go to configuration

--- src/main/kotlin/com/vaadin/plugin/activity/ConfigurationCheckPostStartupProjectActivity.kt
+++ src/main/kotlin/com/vaadin/plugin/activity/ConfigurationCheckPostStartupProjectActivity.kt
@@ -22,9 +22,13 @@ class ConfigurationCheckPostStartupProjectActivity : ProjectActivity {
     companion object {
         const val NOTIFICATION_GROUP = "Vaadin Configuration Check"
 
+        val VCS_CONFIRMATION_CONFIGURABLE =
+            VcsBundle.message("configurable.VcsGeneralConfigurationConfigurable.display.name")
+        val HOTSWAP_CONFIGURABLE = "HotSwap"
+
         val MESSAGE_RELOAD_CLASSES = JavaDebuggerBundle.message("label.debugger.hotswap.configurable.reload.classes")
         val MESSAGE_RELOAD_ALWAYS = JavaDebuggerBundle.message("label.debugger.hotswap.configurable.always")
-        val MESSAGE_VCS_CONFIGURABLE = VcsBundle.message("version.control.main.configurable.name")
+        val MESSAGE_VCS = VcsBundle.message("version.control.main.configurable.name")
         val MESSAGE_WHEN_FILES_CREATED = VcsBundle.message("settings.border.when.files.are.created")
         val MESSAGE_ADD_SILENTLY = VcsBundle.message("radio.after.creation.add.silently")
         val MESSAGE_DO_NOT_ADD = VcsBundle.message("radio.after.creation.do.not.add")
@@ -41,19 +45,19 @@ class ConfigurationCheckPostStartupProjectActivity : ProjectActivity {
         if (dm.RUN_HOTSWAP_AFTER_COMPILE == DebuggerSettings.RUN_HOTSWAP_ASK) {
             notify(
                 project,
-                "Your HotSwap class reload setting requires confirmation each time file is going to be reloaded. " +
+                "Your $HOTSWAP_CONFIGURABLE class reload setting requires confirmation each time file is going to be reloaded. " +
                         "It will cause popups appear while working with Vaadin Copilot. " +
                         "To improve experience please set \"$MESSAGE_RELOAD_CLASSES\" to \"$MESSAGE_RELOAD_ALWAYS\"",
-                "reference.idesettings.debugger.hotswap"
+                HOTSWAP_CONFIGURABLE
             )
         }
 
         if (dm.RUN_HOTSWAP_AFTER_COMPILE == DebuggerSettings.RUN_HOTSWAP_NEVER) {
             notify(
                 project,
-                "Your HotSwap class reload is turned off. It will cause changes are not reflected in your application. " +
+                "Your $HOTSWAP_CONFIGURABLE class reload is turned off. It will cause changes are not reflected in your application. " +
                         "To improve experience please set \"$MESSAGE_RELOAD_CLASSES\" to \"$MESSAGE_RELOAD_ALWAYS\"",
-                "reference.idesettings.debugger.hotswap"
+                HOTSWAP_CONFIGURABLE
             )
         }
     }
@@ -64,33 +68,33 @@ class ConfigurationCheckPostStartupProjectActivity : ProjectActivity {
         if (value == VcsShowConfirmationOption.Value.SHOW_CONFIRMATION) {
             notify(
                 project,
-                "Your $MESSAGE_VCS_CONFIGURABLE settings requires confirmation after creating each new file. " +
+                "Your $MESSAGE_VCS settings requires confirmation after creating each new file. " +
                         "It will cause popups appear while working with Vaadin Copilot. " +
                         "To improve experience please set \"$MESSAGE_WHEN_FILES_CREATED\" to \"$MESSAGE_ADD_SILENTLY\" or \"$MESSAGE_DO_NOT_ADD\"",
-                "project.propVCSSupport.Confirmation"
+                VCS_CONFIRMATION_CONFIGURABLE
             )
         }
     }
 
-    private fun notify(project: Project, content: String, configurableId: String) {
+    private fun notify(project: Project, content: String, configurable: String) {
         val notification = Notification(
             NOTIFICATION_GROUP,
             content,
             NotificationType.INFORMATION
         ).setIcon(VaadinIcons.VAADIN)
-        notification.addAction(createGoToConfigurationAction(project, configurableId))
+        notification.addAction(createGoToConfigurationAction(project, configurable))
         notification.addAction(createDontAskAgainAction())
         Notifications.Bus.notify(notification)
     }
 
     private fun createGoToConfigurationAction(
         project: Project,
-        configurableId: String
+        configurable: String
     ): AnAction {
-        return NotificationAction.create("Go to configuration...", { _, notification ->
+        return NotificationAction.create("Show in settings...") { _, notification ->
             notification.hideBalloon()
-            ShowSettingsUtil.getInstance().showSettingsDialog(project, configurableId)
-        })
+            ShowSettingsUtil.getInstance().showSettingsDialog(project, configurable)
+        }
     }
 
     private fun createDontAskAgainAction(): NotificationAction {
