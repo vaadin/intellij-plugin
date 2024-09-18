4dece5aed139344780b2a87ee23d3f7504ba4b24
marcin <marcin@vaadin.com>
2024-09-18 14:04:23 +0200

    add debug using hotswap to run toolbar

---
+++ src/main/kotlin/com/vaadin/plugin/actions/DebugUsingHotSwapAgentAction.kt
@@ -0,0 +1,18 @@
+package com.vaadin.plugin.actions
+
+import com.intellij.execution.Executor
+import com.intellij.execution.ExecutorRegistry
+import com.intellij.execution.dashboard.actions.ExecutorAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.vaadin.plugin.hotswapagent.HotswapAgentExecutor
+
+class DebugUsingHotSwapAgentAction : ExecutorAction() {
+    override fun update(p0: AnActionEvent, p1: Boolean) {
+
+    }
+
+    override fun getExecutor(): Executor {
+        return ExecutorRegistry.getInstance().getExecutorById(HotswapAgentExecutor.ID)!!
+    }
+
+}
