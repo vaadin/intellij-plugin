058d3bdf4c59e39b3faec18ea289da30909b9075
marcin <marcin@vaadin.com>
2024-09-26 11:33:41 +0200

    chore: add to debugger toolbar, rerun label (#102)
    * change label if action is already running
    
    * add to XDebugger toolbar

--- src/main/kotlin/com/vaadin/plugin/actions/DebugUsingHotSwapAgentAction.kt
+++ src/main/kotlin/com/vaadin/plugin/actions/DebugUsingHotSwapAgentAction.kt
@@ -5,9 +5,19 @@ import com.intellij.execution.ExecutorRegistry
 import com.intellij.execution.dashboard.actions.ExecutorAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.vaadin.plugin.hotswapagent.HotswapAgentExecutor
+import com.vaadin.plugin.utils.VaadinIcons
 
 class DebugUsingHotSwapAgentAction : ExecutorAction() {
-    override fun update(p0: AnActionEvent, p1: Boolean) {}
+    override fun update(event: AnActionEvent, isRunning: Boolean) {
+        if (isRunning) {
+            event.presentation.text = "Rerun using HotSwapAgent"
+            // TODO: https://github.com/vaadin/intellij-plugin/issues/101
+            event.presentation.icon = VaadinIcons.DEBUG_HOTSWAP
+        } else {
+            event.presentation.text = "Debug using HotSwapAgent"
+            event.presentation.icon = VaadinIcons.DEBUG_HOTSWAP
+        }
+    }
 
     override fun getExecutor(): Executor {
         return ExecutorRegistry.getInstance().getExecutorById(HotswapAgentExecutor.ID)!!
