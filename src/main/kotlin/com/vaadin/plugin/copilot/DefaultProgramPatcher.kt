da481cb59f1a82a32cff578f0badd89207fc87e9
Miguel García Martínez <mgarcia@vaadin.com>
2024-10-31 10:58:35 +0000

    dedicated Copilot default ProgramPatcher

---
+++ src/main/kotlin/com/vaadin/plugin/copilot/DefaultProgramPatcher.kt
@@ -0,0 +1,33 @@
+package com.vaadin.plugin.copilot
+
+import com.intellij.execution.Executor
+import com.intellij.execution.configurations.JavaParameters
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.runners.JavaProgramPatcher
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+
+class DefaultProgramPatcher : JavaProgramPatcher() {
+
+    private val LOG: Logger = Logger.getInstance(DefaultProgramPatcher::class.java)
+
+    override fun patchJavaParameters(executor: Executor?, runProfile: RunProfile?, javaParameters: JavaParameters?) {
+
+        // Create plugin present property -DcopilotPluginActive=true
+        if (javaParameters == null) {
+            return
+        }
+        val paramsList = javaParameters.vmParametersList
+
+        if (runProfile is RunConfiguration) {
+            val project: Project = runProfile.project
+            val projectBasePath: String? = project.basePath
+
+            projectBasePath?.let {
+                val dotFilePath = CopilotPluginUtil.getDotFileDirectory(project)?.path + '/' + CopilotPluginUtil.DOTFILE
+                paramsList.add("-DcopilotPluginPath=$dotFilePath")
+            }
+        }
+    }
+}
