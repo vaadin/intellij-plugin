08cfde8cb3163a1bd73869b4aea26d8d8a54a4bd
Artur Signell <artur@vaadin.com>
2024-11-04 13:43:54 +0200

    feat: Include resource paths when fetching source paths

--- src/main/kotlin/com/vaadin/plugin/copilot/handler/GetSourcePathsHandler.kt
+++ src/main/kotlin/com/vaadin/plugin/copilot/handler/GetSourcePathsHandler.kt
@@ -5,19 +5,24 @@ import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ModuleRootManager
 import io.netty.handler.codec.http.HttpResponseStatus
+import org.jetbrains.jps.model.java.JavaResourceRootType
 import org.jetbrains.jps.model.java.JavaSourceRootType
 
 class GetSourcePathsHandler(project: Project) : AbstractHandler(project) {
 
     override fun run(): HandlerResponse {
         val sourcePaths = ArrayList<String>()
-        val testPaths = ArrayList<String>()
+        val testSourcePaths = ArrayList<String>()
+        val resourcePaths = ArrayList<String>()
+        val testResourcePaths = ArrayList<String>()
         ModuleManager.getInstance(project).modules.forEach { module: Module ->
             val moduleRootManager = ModuleRootManager.getInstance(module)
             sourcePaths.addAll(moduleRootManager.getSourceRoots(JavaSourceRootType.SOURCE).map { it.path })
-            testPaths.addAll(moduleRootManager.getSourceRoots(JavaSourceRootType.TEST_SOURCE).map { it.path })
+            testSourcePaths.addAll(moduleRootManager.getSourceRoots(JavaSourceRootType.TEST_SOURCE).map { it.path })
+            resourcePaths.addAll(moduleRootManager.getSourceRoots(JavaResourceRootType.RESOURCE).map { it.path })
+            testResourcePaths.addAll(moduleRootManager.getSourceRoots(JavaResourceRootType.TEST_RESOURCE).map { it.path })
         }
-        val data: Map<String, Any> = mapOf("sourcePaths" to sourcePaths, "testSourcePaths" to testPaths)
+        val data: Map<String, Any> = mapOf("sourcePaths" to sourcePaths, "testSourcePaths" to testSourcePaths, "resourcePaths" to resourcePaths, "testResourcePaths" to testResourcePaths)
         return HandlerResponse(HttpResponseStatus.OK, data)
     }
 }
