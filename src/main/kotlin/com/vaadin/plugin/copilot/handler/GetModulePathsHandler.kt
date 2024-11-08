9a9761975952211cf88ce92a97a484f362c405ab
Artur Signell <artur@vaadin.com>
2024-11-08 10:34:07 +0200

    fix: return source path information per module
    Replaces getSourcePaths with getModulePaths as the API changes

---
+++ src/main/kotlin/com/vaadin/plugin/copilot/handler/GetModulePathsHandler.kt
@@ -0,0 +1,47 @@
+package com.vaadin.plugin.copilot.handler
+
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
+import io.netty.handler.codec.http.HttpResponseStatus
+import org.jetbrains.jps.model.java.JavaResourceRootType
+import org.jetbrains.jps.model.java.JavaSourceRootType
+
+class GetModulePathsHandler(project: Project) : AbstractHandler(project) {
+
+    @JvmRecord
+    data class ModuleInfo(
+        val name: String,
+        val contentRoots: Array<String>,
+        val javaSourcePaths: Array<String>,
+        val javaTestSourcePaths: Array<String>,
+        val resourcePaths: Array<String>,
+        val testResourcePaths: Array<String>
+    )
+
+
+    override fun run(): HandlerResponse {
+        val modules = ArrayList<ModuleInfo>();
+        ModuleManager.getInstance(project).modules.forEach { module: Module ->
+            val moduleRootManager = ModuleRootManager.getInstance(module)
+            val contentRoots = moduleRootManager.contentRoots.map { it.path }
+            val javaSourcePaths = moduleRootManager.getSourceRoots(JavaSourceRootType.SOURCE).map { it.path };
+            val javaTestSourcePaths = moduleRootManager.getSourceRoots(JavaSourceRootType.TEST_SOURCE).map { it.path }
+            val resourcePaths = moduleRootManager.getSourceRoots(JavaResourceRootType.RESOURCE).map { it.path }
+            val testResourcePaths = moduleRootManager.getSourceRoots(JavaResourceRootType.TEST_RESOURCE).map { it.path }
+            modules.add(
+                ModuleInfo(
+                    module.name,
+                    contentRoots.toTypedArray(),
+                    javaSourcePaths.toTypedArray(),
+                    javaTestSourcePaths.toTypedArray(),
+                    resourcePaths.toTypedArray(),
+                    testResourcePaths.toTypedArray()
+                )
+            );
+        }
+        val data = mapOf("modules" to modules)
+        return HandlerResponse(HttpResponseStatus.OK, data)
+    }
+}
