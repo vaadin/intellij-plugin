92c2fd69952379c53270cad9dd4687f17f07b11a
Artur Signell <artur@vaadin.com>
2024-11-08 12:34:52 +0200

    include output path

--- src/main/kotlin/com/vaadin/plugin/copilot/handler/GetModulePathsHandler.kt
+++ src/main/kotlin/com/vaadin/plugin/copilot/handler/GetModulePathsHandler.kt
@@ -3,6 +3,7 @@ package com.vaadin.plugin.copilot.handler
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.CompilerModuleExtension
 import com.intellij.openapi.roots.ModuleRootManager
 import io.netty.handler.codec.http.HttpResponseStatus
 import org.jetbrains.jps.model.java.JavaResourceRootType
@@ -17,7 +18,8 @@ class GetModulePathsHandler(project: Project) : AbstractHandler(project) {
         val javaSourcePaths: Array<String>,
         val javaTestSourcePaths: Array<String>,
         val resourcePaths: Array<String>,
-        val testResourcePaths: Array<String>
+        val testResourcePaths: Array<String>,
+        val outputPath:String?
     )
 
     override fun run(): HandlerResponse {
@@ -25,11 +27,17 @@ class GetModulePathsHandler(project: Project) : AbstractHandler(project) {
         ModuleManager.getInstance(project).modules.forEach { module: Module ->
             val moduleRootManager = ModuleRootManager.getInstance(module)
             val contentRoots = moduleRootManager.contentRoots.map { it.path }
+
+            val compilerModuleExtension = CompilerModuleExtension.getInstance(module)
+
             // Note that the JavaSourceRootType.SOURCE also includes Kotlin source folders
             val javaSourcePaths = moduleRootManager.getSourceRoots(JavaSourceRootType.SOURCE).map { it.path }
             val javaTestSourcePaths = moduleRootManager.getSourceRoots(JavaSourceRootType.TEST_SOURCE).map { it.path }
+
             val resourcePaths = moduleRootManager.getSourceRoots(JavaResourceRootType.RESOURCE).map { it.path }
             val testResourcePaths = moduleRootManager.getSourceRoots(JavaResourceRootType.TEST_RESOURCE).map { it.path }
+
+            val outputPath = compilerModuleExtension?.compilerOutputPath?.path;
             modules.add(
                 ModuleInfo(
                     module.name,
@@ -37,7 +45,8 @@ class GetModulePathsHandler(project: Project) : AbstractHandler(project) {
                     javaSourcePaths.toTypedArray(),
                     javaTestSourcePaths.toTypedArray(),
                     resourcePaths.toTypedArray(),
-                    testResourcePaths.toTypedArray()))
+                    testResourcePaths.toTypedArray(),
+                    outputPath ))
         }
         val data = mapOf("modules" to modules)
         return HandlerResponse(HttpResponseStatus.OK, data)
