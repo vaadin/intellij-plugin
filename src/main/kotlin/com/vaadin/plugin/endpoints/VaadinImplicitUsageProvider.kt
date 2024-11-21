354e8eb8cd47c73a67c5854901544ce0fc4526d2
marcin <marcin@vaadin.com>
2024-11-21 11:46:42 +0000

    feat: Vaadin Endpoints integration (#167)
    * Vaadin Endpoints integration

---
+++ src/main/kotlin/com/vaadin/plugin/endpoints/VaadinImplicitUsageProvider.kt
@@ -0,0 +1,30 @@
+package com.vaadin.plugin.endpoints
+
+import com.intellij.codeInsight.AnnotationUtil
+import com.intellij.codeInsight.daemon.ImplicitUsageProvider
+import com.intellij.lang.jvm.JvmModifier
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiField
+import com.intellij.psi.util.InheritanceUtil
+
+internal class VaadinImplicitUsageProvider : ImplicitUsageProvider {
+    override fun isImplicitUsage(element: PsiElement): Boolean {
+        return element is PsiClass &&
+            !element.isInterface &&
+            !element.isEnum &&
+            !element.hasModifier(JvmModifier.ABSTRACT) &&
+            !element.isAnnotationType &&
+            (AnnotationUtil.isAnnotated(element, VAADIN_ROUTE, 0) ||
+                AnnotationUtil.isAnnotated(element, VAADIN_TAG, 0) ||
+                InheritanceUtil.isInheritor(element, VAADIN_APP_SHELL_CONFIGURATOR))
+    }
+
+    override fun isImplicitRead(element: PsiElement): Boolean {
+        return false
+    }
+
+    override fun isImplicitWrite(element: PsiElement): Boolean {
+        return element is PsiField && AnnotationUtil.isAnnotated(element, VAADIN_ID, 0)
+    }
+}
---
+++ src/main/kotlin/com/vaadin/plugin/endpoints/VaadinRoute.kt
@@ -0,0 +1,7 @@
+package com.vaadin.plugin.endpoints
+
+import com.intellij.psi.PsiAnchor
+
+internal class VaadinRoute(val urlMapping: String, val locationString: String, val anchor: PsiAnchor) {
+    fun isValid(): Boolean = anchor.retrieve() != null
+}
