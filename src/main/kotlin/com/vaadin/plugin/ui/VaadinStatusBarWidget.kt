78391fb422aa557e6dfbb387ec52e32ff5a41464
marcin <marcin@vaadin.com>
2024-12-06 16:03:02 +0100

    Simplify widget

--- src/main/kotlin/com/vaadin/plugin/ui/VaadinStatusBarWidget.kt
+++ src/main/kotlin/com/vaadin/plugin/ui/VaadinStatusBarWidget.kt
@@ -1,19 +1,18 @@
 package com.vaadin.plugin.ui
 
-import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.openapi.ui.popup.ListPopup
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.StatusBarWidget
-import com.intellij.openapi.wm.impl.status.EditorBasedStatusBarPopup
 import com.intellij.ui.BadgeIconSupplier
+import com.intellij.ui.awt.RelativePoint
+import com.intellij.util.Consumer
 import com.vaadin.plugin.copilot.CopilotPluginUtil
 import com.vaadin.plugin.utils.VaadinIcons
 import com.vaadin.plugin.utils.hasEndpoints
+import java.awt.event.MouseEvent
 import javax.swing.Icon
 
-class VaadinStatusBarWidget(project: Project) : EditorBasedStatusBarPopup(project, false) {
+class VaadinStatusBarWidget(private val project: Project) : StatusBarWidget, StatusBarWidget.IconPresentation {
 
     companion object {
         const val ID = "VaadinStatusBarPanel"
@@ -21,33 +20,25 @@ class VaadinStatusBarWidget(project: Project) : EditorBasedStatusBarPopup(projec
 
     private val iconSupplier: BadgeIconSupplier = BadgeIconSupplier(VaadinIcons.VAADIN)
 
-    private var clicked: Boolean = false
-
     override fun ID(): String {
         return ID
     }
 
-    override fun createInstance(project: Project): StatusBarWidget {
-        return VaadinStatusBarWidget(project)
+    override fun getPresentation(): StatusBarWidget.WidgetPresentation {
+        return this
     }
 
-    override fun createPopup(context: DataContext): ListPopup? {
-        clicked = true
-        val popup = VaadinStatusBarInfoPopupPanel(isCopilotActive(), hasEndpoints())
-        JBPopupFactory.getInstance()
-            .createComponentPopupBuilder(popup, null)
-            .createPopup()
-            .showInBestPositionFor(context)
-        return null
+    override fun getClickConsumer(): Consumer<MouseEvent> {
+        return Consumer {
+            val popup = VaadinStatusBarInfoPopupPanel(isCopilotActive(), hasEndpoints())
+            JBPopupFactory.getInstance()
+                .createComponentPopupBuilder(popup, null)
+                .createPopup()
+                .show(RelativePoint.fromScreen(it.locationOnScreen))
+        }
     }
 
-    override fun getWidgetState(file: VirtualFile?): WidgetState {
-        val state = WidgetState(getTooltip(), null, true)
-        state.icon = getIcon()
-        return state
-    }
-
-    private fun getTooltip(): String {
+    override fun getTooltipText(): String {
         if (!isCopilotActive() || !hasEndpoints()) {
             return "There are issues while running Vaadin plugin, click to see details"
         }
@@ -55,11 +46,7 @@ class VaadinStatusBarWidget(project: Project) : EditorBasedStatusBarPopup(projec
         return "Vaadin plugin is active"
     }
 
-    private fun getIcon(): Icon {
-        if (clicked) {
-            return iconSupplier.originalIcon
-        }
-
+    override fun getIcon(): Icon {
         if (!isCopilotActive() || !hasEndpoints()) {
             return iconSupplier.warningIcon
         }
