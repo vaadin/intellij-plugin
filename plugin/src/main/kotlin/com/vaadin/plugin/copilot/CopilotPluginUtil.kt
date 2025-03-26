65aa15e9fcda885555d59da38205efd376f85d1d
marcin <marcin@vaadin.com>
2025-03-26 16:42:52 +0100

    Find by main class name

--- plugin/src/main/kotlin/com/vaadin/plugin/copilot/CopilotPluginUtil.kt
+++ plugin/src/main/kotlin/com/vaadin/plugin/copilot/CopilotPluginUtil.kt
@@ -99,7 +99,7 @@ class CopilotPluginUtil {
                 HANDLERS.GET_MODULE_PATHS.command -> return GetModulePathsHandler(project)
                 HANDLERS.COMPILE_FILES.command -> return CompileFilesHandler(project, data)
                 HANDLERS.RESTART_APPLICATION.command -> return RestartApplicationHandler(project)
-                HANDLERS.RESTART_SERVICE.command -> return RestartServiceHandler(project, data["serviceName"] as String)
+                HANDLERS.RESTART_SERVICE.command -> return RestartServiceHandler(project, data)
                 HANDLERS.RELOAD_MAVEN_MODULE.command ->
                     return ReloadMavenModuleHandler(project, data["moduleName"] as String)
                 else -> {
--- plugin/src/main/kotlin/com/vaadin/plugin/copilot/handler/RestartServiceHandler.kt
+++ plugin/src/main/kotlin/com/vaadin/plugin/copilot/handler/RestartServiceHandler.kt
@@ -1,26 +1,32 @@
 package com.vaadin.plugin.copilot.handler
 
+import com.intellij.execution.process.BaseOSProcessHandler
 import com.intellij.execution.runners.ExecutionUtil
 import com.intellij.execution.ui.RunContentManager
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
 
-class RestartServiceHandler(project: Project, serviceName: String) : AbstractHandler(project) {
+class RestartServiceHandler(project: Project, data: Map<String, Any>) : AbstractHandler(project) {
 
-    private val serviceName: String = serviceName as String
+    private val expectedMainClass: String = data["mainClass"] as String
 
     override fun run(): HandlerResponse {
-        runInEdt {
             val contentManager = RunContentManager.getInstance(project)
-            for (descriptor in contentManager.allDescriptors) {
-                if (descriptor.displayName == serviceName) {
-                    LOG.debug("Restarting ${descriptor.displayName} (${project.name})")
-                    ExecutionUtil.restart(descriptor)
-                    break
+            val allDescriptors = contentManager.allDescriptors
+            for (descriptor in allDescriptors) {
+                var processHandler = descriptor.processHandler
+                if (processHandler is BaseOSProcessHandler) {
+                    var mainClassName = processHandler.commandLine.split(" ").last()
+                    if (mainClassName == expectedMainClass) {
+                        LOG.debug("Restarting ${descriptor.displayName} (${project.name})")
+                        runInEdt {
+                            ExecutionUtil.restart(descriptor)
+                        }
+                        break
+                    }
                 }
             }
-            LOG.debug("Restart of ${project.name} failed - content not found")
-        }
+
         return RESPONSE_OK
     }
 }
