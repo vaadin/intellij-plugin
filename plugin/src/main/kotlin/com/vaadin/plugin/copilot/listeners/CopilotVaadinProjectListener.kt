e54d7ce29d9f1baa784204069095ca55b4f99850
marcin <marcin@vaadin.com>
2025-04-10 08:03:00 -0700

    fix: Detect Vaadin on new projects (#362)

--- plugin/src/main/kotlin/com/vaadin/plugin/copilot/listeners/CopilotVaadinProjectListener.kt
+++ plugin/src/main/kotlin/com/vaadin/plugin/copilot/listeners/CopilotVaadinProjectListener.kt
@@ -1,15 +1,17 @@
 package com.vaadin.plugin.copilot.listeners
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.vaadin.plugin.copilot.CopilotPluginUtil.Companion.saveDotFile
+import com.vaadin.plugin.copilot.service.CopilotDotfileService
 import com.vaadin.plugin.listeners.VaadinProjectListener
 import com.vaadin.plugin.ui.VaadinStatusBarWidget
 
 class CopilotVaadinProjectListener : VaadinProjectListener {
 
     override fun vaadinProjectDetected(project: Project) {
-        if (!project.isDisposed) {
+        if (!project.isDisposed && !project.service<CopilotDotfileService>().isActive()) {
             saveDotFile(project)
             DumbService.getInstance(project).smartInvokeLater { VaadinStatusBarWidget.update(project) }
         }
