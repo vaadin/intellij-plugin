72e98cd6bf99852d42c5bc0348cd283a34df0b32
Miguel García Martínez <mgarcia@vaadin.com>
2025-07-14 14:04:27 +0200

    Downloading chat artifacts

--- plugin/src/main/kotlin/com/vaadin/plugin/copilot/listeners/CopilotVaadinProjectListener.kt
+++ plugin/src/main/kotlin/com/vaadin/plugin/copilot/listeners/CopilotVaadinProjectListener.kt
@@ -3,6 +3,10 @@ package com.vaadin.plugin.copilot.listeners
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
+import com.vaadin.plugin.copilot.CopilotPluginUtil.Companion.downloadLatestCopilotChatJar
+import com.vaadin.plugin.copilot.CopilotPluginUtil.Companion.downloadLatestCopilotLocalMcpServerJar
+import com.vaadin.plugin.copilot.CopilotPluginUtil.Companion.existsLatestCopilotChatJar
+import com.vaadin.plugin.copilot.CopilotPluginUtil.Companion.existsLatestCopilotLocalMcpServerJar
 import com.vaadin.plugin.copilot.CopilotPluginUtil.Companion.saveDotFile
 import com.vaadin.plugin.copilot.CopilotPluginUtil.Companion.startChatApp
 import com.vaadin.plugin.copilot.service.CopilotDotfileService
@@ -14,7 +18,26 @@ class CopilotVaadinProjectListener : VaadinProjectListener {
     override fun vaadinProjectDetected(project: Project) {
         if (!project.isDisposed && !project.service<CopilotDotfileService>().isActive()) {
             saveDotFile(project)
-            startChatApp(project)
+            if (existsLatestCopilotChatJar() && existsLatestCopilotLocalMcpServerJar()) {
+                startChatApp(project)
+            } else {
+                when {
+                    !existsLatestCopilotChatJar() && !existsLatestCopilotLocalMcpServerJar() -> {
+                        // Download latest chat jar
+                        downloadLatestCopilotChatJar(project).thenRun {
+                            downloadLatestCopilotLocalMcpServerJar(project).thenRun { startChatApp(project) }
+                        }
+                    }
+                    existsLatestCopilotChatJar() && !existsLatestCopilotLocalMcpServerJar() -> {
+                        // Download latest local MCP server jar
+                        downloadLatestCopilotLocalMcpServerJar(project).thenRun { startChatApp(project) }
+                    }
+                    !existsLatestCopilotChatJar() && existsLatestCopilotLocalMcpServerJar() -> {
+                        // Download latest chat jar
+                        downloadLatestCopilotChatJar(project).thenRun { startChatApp(project) }
+                    }
+                }
+            }
             DumbService.getInstance(project).smartInvokeLater { VaadinStatusBarWidget.update(project) }
         }
     }
