54cf967c1cecadb2f65b4901b8464c1556517781
Miguel García Martínez <mgarcia@vaadin.com>
2025-07-29 01:39:56 +0200

    Added addon example

--- plugin-api-test-client/build.gradle.kts
+++ plugin-api-test-client/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
     implementation("com.vaadin:vaadin-core:24.6.5")
     implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
+    implementation("com.flowingcode.vaadin.addons:day-of-week-selector-addon:1.1.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.2")
 }
 
--- plugin-api-test-client/src/test/java/com/plugin/testviews/CustomView.java
+++ plugin-api-test-client/src/test/java/com/plugin/testviews/CustomView.java
@@ -1,7 +1,9 @@
 package com.plugin.testviews;
 
+import com.flowingcode.vaadin.addons.dayofweekselector.DayOfWeekSelector;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+
 
 /**
  * A simple Vaadin view that can be used for testing get routes tool within this project.
@@ -9,4 +11,7 @@ import com.vaadin.flow.router.Route;
  */
 @Route(value = "custom", autoLayout = false)
 public class CustomView extends VerticalLayout {
+    public CustomView() {
+        add(new DayOfWeekSelector());
+    }
 }
